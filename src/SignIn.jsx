8295715a1f4b3aed12bae7055f9a5401dc475572
TIMM SNOW <two.tone.snow@gmail.com>
2023-02-25 10:06:43 -1000

    updates dependencies and adds axios login functionality

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import { Formik } from 'formik'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,6 +7,30 @@ import Col from 'react-bootstrap/Col';
 
 const SignIn = (props) => {
   const event = props.event
+
+  const logIn = (values) => {
+    let username = values["email"];
+    let password = values["password"]
+
+    const token = `${username}:${password}`;
+    const encodedToken = window.btoa(token)
+    const session_url = 'http://localhost:3000/api-keys';
+
+    let config = {
+      method: 'post',
+      url: session_url,
+      headers: { 'Authorization': 'Basic '+ encodedToken }
+    };
+
+    axios(config)
+    .then(function (response) {
+      window.localStorage.setItem('token', response.data["token"])
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   return (
     <Container>
       <Formik
@@ -22,6 +47,7 @@ const SignIn = (props) => {
         return errors;
       }}
       onSubmit={(values, { setSubmitting }) => {
+        logIn(values)
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
           setSubmitting(false);
